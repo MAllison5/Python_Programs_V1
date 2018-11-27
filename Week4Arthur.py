3e40e6fda5545d03965bba77768bf11ff412da4c
MAllison5 <43646239+MAllison5@users.noreply.github.com>
2018-11-27 15:44:28 +0000

    Add files via upload

---
+++ Week4Arthur.py
@@ -0,0 +1,5 @@
+#Matthew Allison - U1865262@hud.ac.uk#
+'''Capitalizing the first letter and keeping lower case'''
+
+name = (input('Hello, who are you?'))
+print('Hello', name.capitalize(), 'its is good to meet you.')
